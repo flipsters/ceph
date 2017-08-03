5ad5481ae1b565f9ef1ebf56941965e7bf834fee
Abhishek Varshney <abhishek.varshney@flipkart.com>
2017-08-03 11:54:09 +0000

    rgw : fix race in RGWCompleteMultipart. Backport of dd7dc62eaddb47447c01c03c87ad56cd7690693e from ceph/ceph
    Signed-off-by: Abhishek Varshney <abhishek.varshney@flipkart.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1169,6 +1169,7 @@ class RGWRados
   friend class RGWGC;
   friend class RGWStateLog;
   friend class RGWReplicaLogger;
+  friend class RGWCompleteMultipart;
 
   /** Open the pool used as root for this gateway */
   int open_root_pool_ctx();
